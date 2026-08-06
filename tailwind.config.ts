43ced403de0199fda54e6827b7e82f3ebdacaaa7
Dhafi <dhafi.balikpapan@gmail.com>
2026-08-06 12:32:56 +0700

    tambah color pallet PKKMB

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        ember : "#F5E26C",
+        twilight : "#801831",
+        inferno : "#C71A2D",
       },
     },
   },
